472c4a1daf04471809379a7cebfec0f4aa220b63
Terence Hughes <Terencehughes450@gmail.com>
2023-07-29 17:59:29 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Terence Hughes';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
